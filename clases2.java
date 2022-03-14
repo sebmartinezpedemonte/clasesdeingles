657da00e03e625b818b248a7536eaf62e5ff6c9f
Sebastian Martinez Pedemonte <sebastianmartinezpedemonte@gmail.com>
2022-03-14 13:53:28 -0300

    eliminated sample changes

--- clases2.java
+++
@@ -1,6 +0,0 @@
-package clases.ingles;
-public class clases2 {
-    public static void main(String[] args) {
-        System.out.println("pop2");
-    }
-}
--- dir1/pop.java
+++
@@ -1,7 +0,0 @@
-package clases.ingles.dir1;
-
-public class pop {
-    public static void main(String[] args) {
-        System.out.println("pop");
-    }
-}
